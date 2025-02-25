c6d98dca7ee8f08688ebf5d117298f3511159abb
nucash-mining <nucash.mining@gmail.com>
2025-02-25 04:34:04 -0600

    Add ethash package and updates for Altcoinchain fork

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -19,8 +19,9 @@ package ethconfig
 
 import (
 	"errors"
+	"math/big"
 	"time"
-
+        "github.com/nucash-mining/go-altcoinchain/go-ethereum/consensus/ethash"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/consensus/beacon"
@@ -162,18 +163,20 @@ type Config struct {
 }
 
 // CreateConsensusEngine creates a consensus engine for the given chain config.
-// Clique is allowed for now to live standalone, but ethash is forbidden and can
-// only exist on already merged networks.
 func CreateConsensusEngine(config *params.ChainConfig, db ethdb.Database) (consensus.Engine, error) {
-	// If proof-of-authority is requested, set it up
-	if config.Clique != nil {
-		return beacon.New(clique.New(config.Clique, db)), nil
-	}
-	// If defaulting to proof-of-work, enforce an already merged network since
-	// we cannot run PoW algorithms anymore, so we cannot even follow a chain
-	// not coordinated by a beacon node.
-	if !config.TerminalTotalDifficultyPassed {
-		return nil, errors.New("ethash is only supported as a historical component of already merged networks")
-	}
-	return beacon.New(ethash.NewFaker()), nil
+    // Clique is allowed for now to live standalone but ethash is forbidden and can
+    // only exist on already merged networks.
+    if config.Clique != nil {
+        return beacon.New(clique.New(config.Clique, db)), nil
+    }
+    // If defaulting to proof-of-work, enforce an already merged network since
+    // we cannot run PoW algorithms anymore, so we cannot even follow a chain
+    // not coordinated by a beacon node.
+    // Modified for custom chain ID 314156 to allow Ethash PoW
+    if config.ChainID.Cmp(big.NewInt(314156)) == 0 {
+        return ethash.New(), nil // Return real Ethash for PoW mining
+    } else if !config.TerminalTotalDifficultyPassed {
+        return nil, errors.New("ethash is only supported as a historical component of already merged networks")
+    }
+    return beacon.New(ethash.NewFaker()), nil
 }
