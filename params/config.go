ec76fb67d88b0ea6a9750769ba72eb185b718aa9
NuCash <39842364+nucash-mining@users.noreply.github.com>
2025-10-03 14:15:49 -0600

    Update config.go

--- params/config.go
+++ params/config.go
@@ -24,7 +24,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"golang.org/x/crypto/sha3"
 )
-
+var ChainID = big.NewInt(2330)
 // Genesis hashes to enforce below configs on.
 var (
 	MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
